ccfa3b82981d6150d3c83037b87bb38e8a989d84
Ayoub Maatouch <aymaatou@e1r12p4.1337.ma>
2021-06-07 21:44:47 +0100

    Still Working on it

--- pipex.c
+++ pipex.c
@@ -12,18 +12,49 @@
 
 #include "pipex.h"
 
+t_data *get_last_node(t_data *tmp)
+{
+	while (tmp)
+	{
+		if (tmp->next == NULL)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
+t_data *get_cmd(char *av)
+{
+    t_data *cmds;
 
+    cmds = (t_data*) malloc(sizeof(t_data));
+    cmds->next = NULL;
+    cmds->cmd = ft_split(av, ' ');
+    return cmds;
+}
+
+t_data *ge_data(int ac , char **av)
+{
+    int i = 2;
+    t_data *data;
+    t_data *tmp;
+    while (i < (ac - 2))
+    {
+        if (i == 2)
+            data = get_cmd(av[i]);
+        else
+            tmp = get_last_node(data);
+        tmp->next = get_cmd(av[i]);
+        i++;
+    }
+    tmp->next = NULL;
+    return data;
+}
+n 
 
 int main(int ac, char **av, char **env)
 {
-    
+    puts(av[1]);
+    get_data(ac, av);
+    puts(av[ac - 1]);
 
-  const char *ls[] = { "cat", NULL };
-  const char *awk[] = { "ls", NULL };
-  const char *sort[] = { "ls", NULL };
-  const char *uniq[] = { "ls", NULL };
-  struct command cmd [] = { {ls}, {awk},  {sort}, {uniq} };
-
-  return 
-  fork_pipes (4, cmd);
 }
--- pipex.h
+++ pipex.h
@@ -1,3 +1,4 @@
+#include "libft/library/libft.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,5 +8,11 @@ struct command
 {
   const char **argv;
 };
+
+typedef struct s_data {
+    char **cmd;
+    struct s_data *next;
+
+} t_data;
 
 int fork_pipes (int n, struct command *cmd);
